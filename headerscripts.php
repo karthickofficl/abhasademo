5ad64cb9b166e6a352f77d619fbf669973a9808e
karthickofficl <karthikraja@akkenna.com>
2024-07-18 15:16:15 +0530

    Some changes push on 18-07-24

--- headerscripts.php
+++ headerscripts.php
@@ -1,6 +1,4 @@
-<!-- Meta Pixel Code -->
-   <script>
-!function(f,b,e,v,n,t,s)
+<script>!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -10,8 +8,6 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '402258139214044');
 fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
+</script><noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=402258139214044&ev=PageView&noscript=1"
 /></noscript>
-<!-- End Meta Pixel Code -->
